a545a3481dc30a92e6ec59aaf3fcb9553a482e32
Liewe Gutter <lgutter@student.codam.nl>
2019-10-21 17:01:04 +0200

    improvement: added input and comment to checkInput
    added comments to explain what checkInput does,
    improved readability a bit, and added verification of the input string.

--- checkInput.py
+++ checkInput.py
@@ -1,4 +1,13 @@
-def checkInput(config):
+def checkInput(config, input_):
+    '''
+    checkInput verifies if the the input is valid,
+    and if the contents of the json file are what we expect.
+    It verifies if every entry is of the right type, and where possible,
+    it verifies that the content of fields are what we expect them to be.
+    for example, it checks if everything in alphabet is a single character.
+    When something is wrong, it raises a ValueError with a relevant message.
+    '''
+
     expected = {'name': str,
                 'alphabet': list,
                 'blank': str,
@@ -7,38 +16,68 @@ def checkInput(config):
                 'finals': list,
                 'transitions': dict}
     if set(expected.keys()) != set(config.keys()):
-        raise ValueError("incorrect keys in json")
+        raise ValueError(f"""incorrect keys in json.
+    expected: {expected.keys()}
+    actual:   {config.keys()}""")
+
     for key in config:
         if type(config[key]) != expected[key]:
-            raise ValueError("{0} should be {1}, but is {2}!"
-                             .format(key, expected[key], type(config[key])))
+            raise ValueError(
+                f"{key} should be {expected[key]} but is {type(config[key])}!")
+
     if config['blank'] not in config['alphabet']:
         raise ValueError("'blank' should be in 'alphabet'!")
+
     for key in config['alphabet']:
         if len(key) != 1:
-            raise ValueError("'" + key + "'" +
-                             " in 'alphabet' should be a single character!")
+            raise ValueError(
+                f"'{key}' in 'alphabet' should be a single character!")
+
+# check the input now, after we've verified the alphabet and blank.
+    for c in input_:
+        if c not in config['alphabet']:
+            raise ValueError(f"'{c}' in 'input' should be in 'alphabet'")
+
+    if config['blank'] in input_:
+        raise ValueError(
+            f"'blank' ('{config['blank']}') should not be in input!")
+
+# continue checking the other fields in the json
     if config['initial'] not in config['states']:
         raise ValueError("'initial' should be in 'states'!")
+
     for key in config['finals']:
         if key not in config['states']:
-            raise ValueError(key + "in 'finals' is not in 'states'!")
+            raise ValueError(
+                f"'{key}' in 'finals' is not in 'states'!")
+
     expected = {'read': config['alphabet'],
                 'to_state': config['states'],
                 'write': config['alphabet'],
                 'action': ['LEFT', 'RIGHT']}
     for key in config['transitions'].items():
         if key[0] not in config['states']:
-            raise ValueError(key[0] + " in 'transitions' is not in 'states'!")
+            raise ValueError(
+                f"'{key[0]}' in 'transitions' is not in 'states'!")
+
         if len(key[1]) < 1:
-            raise ValueError(key[0] + " does not have any instructions!")
+            raise ValueError(
+                f"'{key[0]}' in 'transitions' does not have any content!")
+
         for item in key[1]:
+            if len(item.items()) < 1:
+                raise ValueError(
+                    f"'{key[0]}' in 'transitions' has empty block!")
+
             if type(item) != dict:
-                raise ValueError("expected {0}, found {1} in '{2}'!"
-                                 .format(dict, type(item), key[0]))
+                raise ValueError(
+                    f"expected {dict}, found {type(item)} in '{key[0]}'!")
+
             for instr in item.items():
                 if instr[0] not in expected:
-                    raise ValueError(instr[0] + "is not a valid instruction!")
+                    raise ValueError(
+                        f"'{instr[0]}' is not a valid instruction!")
+
                 if instr[1] not in expected[instr[0]]:
-                    raise ValueError("'{0}' should be one of the following:{1}"
-                                     .format(instr[1], expected[instr[0]]))
+                    raise ValueError(
+                        f"'{instr[1]}' should be one of: {expected[instr[0]]}")
