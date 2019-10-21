b482cc299368a6188f5de185fd58f42202e1baf6
Liewe Gutter <lgutter@student.codam.nl>
2019-10-21 21:45:47 +0200

    feature: basic working version of machine
    made a basic working version of the turing machine,
    but it's still missing a lot of error checking and features.
    also changed exit mode to return statement in main,
    but i'm still not sure if this is the best way.

--- ft_turing.py
+++ ft_turing.py
@@ -2,12 +2,31 @@ import json
 import sys
 import argparse
 from checkInput import checkInput
+from printMachine import printMachine
+from printStatus import printStatus
 
 
-def turingMachine(config, input):
-    (name, alphabet, blank, states,
-        initial, finals, transitions) = config.values()
-    print(name)
+class MachineError(Exception):
+    pass
+
+
+def turingMachine(config, input_):
+    state = config['initial']
+    tape = list(input_)
+    transitions = config['transitions']
+    i = 0
+    while state not in config['finals']:
+        for item in transitions[state]:
+            if item['read'] == tape[i]:
+                printStatus(tape, i)
+                tape[i] = item['write']
+                state = item['to_state']
+                i = i - 1 if item['action'] == 'LEFT' else i + 1
+                break
+        else:
+            raise MachineError(
+                f"No valid transition found for state '{state}' and character '{item['read']}'")
+    printStatus(tape, i)
 
 
 def main():
@@ -22,16 +41,19 @@ def main():
         with open(args.jsonfile, 'r') as conf_file:
             config = json.load(conf_file)
     except ValueError as error:
-        print("Error while loading json file: {0}".format(error))
-        sys.exit(1)
+        print(f"Error while loading json file: {error}")
+        return
 
     try:
         checkInput(config, args.input)
     except ValueError as error:
-        print("Error in checkInput: {0}".format(error))
-        exit
-    else:
+        print(f"Error in checkInput: {error}")
+        return
+    printMachine(config)
+    try:
         turingMachine(config, args.input)
+    except MachineError as error:
+        print(f"Error in turingMachine: {error}")
 
 
 if __name__ == "__main__":
