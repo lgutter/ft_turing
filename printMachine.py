bb3e007e02621e65f98a6c9f445c93c6a500fa22
Liewe Gutter <lgutter@student.codam.nl>
2019-10-21 17:45:54 +0200

    feature: added printMachine
    added a function to print the machine definition in a human readable format.

---
+++ printMachine.py
@@ -0,0 +1,20 @@
+def printMachine(config):
+    '''
+    printMachine simply prints the machine definition from the json in a
+    human readable format. For the transitions I use a for loop to print
+    every transition on a seperate line with the needed information.
+    '''
+    print(f"""******************************************
+Machine name: {config['name']}
+******************************************
+Alphabet: {config['alphabet']}
+Blank: '{config['blank']}'
+States: {config['states']}
+Initial: '{config['initial']}'
+Finals: {config['finals']}
+******************************************
+Transitions:""")
+    for key in config['transitions'].items():
+        for item in key[1]:
+            print(
+                f"({key[0]}, {item['read']}) -> ({item['to_state']}, {item['write']}, {item['action']})")
